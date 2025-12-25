93264305f14c9fd707f6a179f0fc427c9eac316e
Yafet Tegbaru <tegbaruyafet18@gmail.com>
2025-12-25 16:51:05 -0500

    Change size of border

--- app/analyze/page.tsx
+++ app/analyze/page.tsx
@@ -2,13 +2,13 @@ import Header from "../components/Header";
 
 export default function page() {
   return <div>
-    <div className="w-full mb-4 border-4 pb-4">
+    <div className="w-full mb-5 border-4 pb-4">
       <Header />
     </div>
-    <div className="w-full mb-4 border-4 pb-4">
+    <div className="w-full mb-5 border-4 pb-4">
       <h1>Analyze Section</h1>
     </div>
-    <div className="w-full mb-4 border-4 pb-4">
+    <div className="w-full mb-5 border-4 pb-4">
       <h1>Steps/Dashboard Section</h1>
     </div>
   </div>
