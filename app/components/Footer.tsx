07b96cb67736f30518568c5669f5f2f6e3ae41e3
Yafet Tegbaru <tegbaruyafet18@gmail.com>
2025-12-29 02:40:07 -0500

    Finished Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,103 +3,125 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <footer>
-            <div className="w-full flex flex-row items-center justify-between px-[60px] py-[20px] gap-[75px] bg-[#000000]">
+            <div className="w-full flex flex-col items-top gap-[20px] px-[60px] py-[20px] bg-[#000000]">
 
-                <div className="flex flex-row">
-                    <Image
-                        src="/images/Logo+Text.png"
-                        alt="Logo"
-                        width={100}
-                        height={100}
-                        className="border-l-[0.5px] border-t-[0.5px] border-b-[0.5px] border-[#00B0F2]"
-                    />
-                    <Image
-                        src="/images/SeeThroughTheNoise.png"
-                        alt="Logo"
-                        width={445}
-                        height={100}
-                        className="border-r-[0.5px] border-t-[0.5px] border-b-[0.5px] border-[#00B0F2]"
-                    />
-                </div>
+                {/* Top Section */}
+                <div className="w-full flex flex-row justify-between items-start">
 
-                {/* Links Section */}
-                <div className="flex flex-row gap-[50px]">
-                    {/* Quick Links */}
-                    <div className="flex flex-col pt-[8px] pb-[8px] gap-[15px]">
-                        <p className="text-[15px] underline font-bold text-[#00B0F2] sf-pro ">Quick Links</p>
-                        <div className="flex flex-col justify-between gap-[10px]">
-                            <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Home</p>
-                            <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">How It Works</p>
-                            <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Analyze</p>
-                            <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Dashboard</p>
-                            <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Feedback</p>
-                        </div>
-                    </div>
-
-                    {/* Policies */}
-                    <div className="flex flex-col pt-[8px] pb-[8px] gap-[15px]">
-                        <p className="text-[15px] underline font-bold text-[#00B0F2] sf-pro ">Policies</p>
-                        <div className="flex flex-col justify-between gap-[10px]">
-                            <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Terms</p>
-                            <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Privacy Policy</p>
-                        </div>
-                    </div>
-
-                    {/* Contact */}
-                    <div className="flex flex-col pt-[8px] pb-[8px] gap-[15px]">
-                        <p className="text-[15px] underline font-bold text-[#00B0F2] sf-pro ">Contact</p>
-                        <div className="flex flex-col justify-between gap-[10px]">
-                            <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Email</p>
-                            <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Phone</p>
-                        </div>
-                    </div>
-
-                </div>
-
-                {/* Social Media Icons */}
-                <div className="flex flex-row items-center pt-[8px] pb-[5px] gap-[20px]">
-                    <Image
-                        src="/images/github.png"
-                        alt="GitHub"
-                        width={24}
-                        height={24}
-                    />
-                    <Image
-                        src="/images/linkedin.png"
-                        alt="LinkedIn"
-                        width={24}
-                        height={24}
-                    />
-                    <Image
-                        src="/images/instagram.png"
-                        alt="Instagram"
-                        width={24}
-                        height={24}
-                    />
-
-                    <div className="flex flex-row pt-[5px] pb-[5px] gap-[5px]">
-
+                    {/* Logo Section */}
+                    <div className="flex flex-row">
                         <Image
-                            src="/images/Y.png"
-                            alt="Y Logo"
-                            width={16}
-                            height={16}
+                            src="/images/Logo+Text.png"
+                            alt="Logo"
+                            width={100}
+                            height={100}
+                            className="border-l-[0.5px] border-t-[0.5px] border-b-[0.5px] border-[#00B0F2]"
                         />
-
                         <Image
-                            src="/images/T.png"
-                            alt="T Logo"
-                            width={16}
-                            height={16}
+                            src="/images/SeeThroughTheNoise.png"
+                            alt="Logo"
+                            width={445}
+                            height={100}
+                            className="border-r-[0.5px] border-t-[0.5px] border-b-[0.5px] border-[#00B0F2]"
                         />
                     </div>
+
+                    {/* Links Section */}
+                    <div className="flex flex-row gap-[50px]">
+                        {/* Quick Links */}
+                        <div className="flex flex-col pt-[8px] pb-[8px] gap-[15px]">
+                            <p className="text-[15px] underline font-bold text-[#00B0F2] sf-pro ">Quick Links</p>
+                            <div className="flex flex-col justify-between gap-[10px]">
+                                <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Home</p>
+                                <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">How It Works</p>
+                                <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Analyze</p>
+                                <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Dashboard</p>
+                                <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Feedback</p>
+                            </div>
+                        </div>
+
+                        {/* Policies */}
+                        <div className="flex flex-col pt-[8px] pb-[8px] gap-[15px]">
+                            <p className="text-[15px] underline font-bold text-[#00B0F2] sf-pro ">Policies</p>
+                            <div className="flex flex-col justify-between gap-[10px]">
+                                <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Terms</p>
+                                <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Privacy Policy</p>
+                            </div>
+                        </div>
+
+                        {/* Contact */}
+                        <div className="flex flex-col pt-[8px] pb-[8px] gap-[15px]">
+                            <p className="text-[15px] underline font-bold text-[#00B0F2] sf-pro ">Contact</p>
+                            <div className="flex flex-col justify-between gap-[10px]">
+                                <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Email</p>
+                                <p className="text-[15px] font-bold text-[#0181B1] sf-pro ">Phone</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* Social Media Icons */}
+                    <div className="flex flex-row items-center pt-[8px] pb-[5px] gap-[20px]">
+                        <Image
+                            src="/images/github.png"
+                            alt="GitHub"
+                            width={24}
+                            height={24}
+                        />
+                        <Image
+                            src="/images/linkedin.png"
+                            alt="LinkedIn"
+                            width={24}
+                            height={24}
+                        />
+                        <Image
+                            src="/images/instagram.png"
+                            alt="Instagram"
+                            width={24}
+                            height={24}
+                        />
+
+                        <div className="flex flex-row pt-[5px] pb-[5px] gap-[5px]">
+
+                            <Image
+                                src="/images/Y.png"
+                                alt="Y Logo"
+                                width={16}
+                                height={16}
+                            />
+
+                            <Image
+                                src="/images/T.png"
+                                alt="T Logo"
+                                width={16}
+                                height={16}
+                            />
+                        </div>
+                    </div>
+
                 </div>
 
-                {}
-                <div>
+                {/* Bottom Section */}
+                <div className="w-full flex flex-col justify-between">
+                    {/* Divider */}
+                    <div className="w-full border-[1px] border-[#003446]" />
 
+                    {/* Info */}
+                    <div className="w-full flex flex-row justify-between items-center pt-[3px] pb-[3px]">
+
+                        {/* Name */}
+                        <div className="flex flex-row gap-[10px]">
+                            <p className="text-[16px] font-normal text-[#004159] sf-pro ">Designed and Developed by Yafet Tegbaru</p>
+                        </div>
+
+                        {/* Copyright */}
+                        <div className="flex flex-row gap-[10px]">
+                            <p className="text-[16px] font-normal text-[#004159] sf-pro ">© 2025 TruthLens. All rights reserved.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </footer>
+
+        </footer >
     );
 }
