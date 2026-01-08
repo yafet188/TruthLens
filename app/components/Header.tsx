2ffd149e42e5092e4f4844edf9d37bd0be8f49fa
Yafet Tegbaru <tegbaruyafet18@gmail.com>
2026-01-07 23:31:15 -0500

    Updated Header Home Link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,27 +1,31 @@
 import Image from 'next/image'
+import Link from 'next/link'
+
 
 export default function Header() {
     return (
         <header>
             <div className="w-full flex flex-row justify-between items-center px-[60px] py-[50px] gap-auto">
-                <Image
-                    src="/images/Logo.png"
-                    alt="Logo"
-                    width={60}
-                    height={60}
-                    className="w-[63px] h-[63px] rounded-[15px] border-[2px] border-solid border-[#00B0F2] shadow-[0px_4px_10px_rgba(0,_0,_0,_0.60)]"
-                />
+                <Link href="/analyze" aria-label="Go to homepage">
+                    <Image
+                        src="/images/Logo.png"
+                        alt="TruthLens Logo"
+                        width={60}
+                        height={60}
+                        className="w-[63px] h-[63px] rounded-[15px] border-[2px] border-[#00B0F2] shadow-[0px_4px_10px_rgba(0,0,0,0.60)] cursor-pointer"
+                    />
+                </Link>
                 <div className='flex flex-row gap-[10px]'>
                     <div className="w-[100px] h-[50px] flex flex-row justify-center items-center rounded-[25px] border-[1px] border-solid border-[#00B0F2] gap-[15px] shadow-[0px_4px_10px_rgba(0,_0,_0,_0.25)]">
                         <p className="text-[24px] font-normal text-[#00B3F4] sf-pro">Login</p>
                     </div>
                     <div className='w-[50px] h-[50px] flex flex-col items-center justify-between rounded-[10px] px-[20px] py-[12px]  border-[1px] border-solid border-[#00B0F2] shadow-[0px_4px_10px_rgba(0,_0,_0,_0.25)] relative'>
-                        <div className='w-[30px] border-solid border-[1.5px] border-[#00B3F4] [0px_4px_10px_rgba(0,_0,_0,_0.50)]'/>
-                        <div className='w-[30px] border-solid border-[1.5px] border-[#00B3F4] [0px_4px_10px_rgba(0,_0,_0,_0.50)]'/>
-                        <div className='w-[30px] border-solid border-[1.5px] border-[#00B3F4] [0px_4px_10px_rgba(0,_0,_0,_0.50)]'/>
+                        <div className='w-[30px] border-solid border-[1.5px] border-[#00B3F4] shadow-[0px_4px_10px_rgba(0,_0,_0,_0.50)]' />
+                        <div className='w-[30px] border-solid border-[1.5px] border-[#00B3F4] shadow-[0px_4px_10px_rgba(0,_0,_0,_0.50)]' />
+                        <div className='w-[30px] border-solid border-[1.5px] border-[#00B3F4] shadow-[0px_4px_10px_rgba(0,_0,_0,_0.50)]' />
                     </div>
                 </div>
             </div>
-        </header> 
+        </header>
     );
 }
