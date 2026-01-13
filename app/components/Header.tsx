fac048076500032a2230532ac8c5477e572b5af9
Yafet Tegbaru <tegbaruyafet18@gmail.com>
2026-01-13 13:21:36 -0500

    Updated Components

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,9 @@ import Link from 'next/link'
 export default function Header() {
     return (
         <header>
+            {/* Header Container */}
             <div className="w-full flex flex-row justify-between items-center px-[60px] py-[50px] gap-auto">
+                {/* Logo */}
                 <Link href="/analyze" aria-label="Go to homepage">
                     <Image
                         src="/images/Logo.png"
@@ -15,6 +17,8 @@ export default function Header() {
                         className="w-[63px] h-[63px] rounded-[15px] border-[2px] border-[#00B0F2] shadow-[0px_4px_10px_rgba(0,0,0,0.60)] cursor-pointer"
                     />
                 </Link>
+
+                {/* Navigation Links */}
                 <div className='flex flex-row gap-[10px]'>
                     <Link href="/signin" aria-label="Go to sign in page">
                         <div className="group w-[100px] h-[50px] flex flex-row justify-center items-center rounded-[25px] border-[1px] border-solid border-[#00B0F2] gap-[15px] hover:bg-[#00B0F2] cursor-pointer transition-all duration-200">
