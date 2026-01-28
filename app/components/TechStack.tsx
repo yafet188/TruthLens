b994a56b0e22bf82a810ed79e7504574127035ba
Yafet Tegbaru <tegbaruyafet18@gmail.com>
2026-01-27 23:09:36 -0500

    Restarting Component

--- app/components/TechStack.tsx
+++ app/components/TechStack.tsx
@@ -5,11 +5,13 @@ import { use, useEffect, useState } from 'react'
 
 export default function TechStack() {
     return (
-        <div className="w-full flex flex-col justify-center items-center px-[40px] py-[40px] gap-[20px]">
+        <div className="w-[990px] flex flex-row justify-between">
+            {/* Reliability */}
             <h2 className="text-[32px] font-semibold text-[#000000] sf-pro">
-                Our Tech Stack
+                Our Tech Stack 
             </h2>
         </div>
     );
 }
+
 
