f23dbd12b26f72f6248d5f3c73a9ea836fd68be7
Yafet Tegbaru <tegbaruyafet18@gmail.com>
2026-01-29 23:27:43 -0500

    Chenged Contents

--- app/analyze/page.tsx
+++ app/analyze/page.tsx
@@ -1,9 +1,6 @@
-// Analyze page entry point for the application, rendering the main layout for the Analyze route.
-
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-// Defines the Analyze route component
 export default function page() {
   return (
     // Sets the background color and minimum height of the page
