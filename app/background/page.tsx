388ae77353367ebca30e45c7ef35d8bd2d7899d2
Yafet Tegbaru <tegbaruyafet18@gmail.com>
2026-01-31 23:59:05 -0500

    Added Filters Component

--- app/background/page.tsx
+++ app/background/page.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Filters from "../components/Filters";
 
 export default function Page() {
   return (
-    <>
+     <div className="bg-[#000F14] min-h-screen">
       <Header />
+      <Filters />
       <Footer />
-    </>
+    </div>
   );
 }
